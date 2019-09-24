68ba5a8f8d82706d4581ad3c7011c8f60747100d
unknown <hhe@mailfence.com>
2019-09-24 17:27:48 -0400

    tinkering to image rendering

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const Container = styled.div
+const PopularList = styled.div
 `
-  background: red;
-  color: white;
+  background: #212025;
+  
 `;
 
-const Header = styled.h2
+const Image = styled.img
 `
-  color: ${props => props.isEven ? 'green' : 'blue' };
+background-image: url("http://imageurlhere.com");
 `;
 
 const api_key = 'd2788c89c4f55d19e63381c2d04593df';
 const api_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 const api_tv = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US&page=1`;
-// https://image.tmdb.org/t/p/w500/6ihyJWRLEngSnlW8HKeDOH3AfSQ.jpg
+const img_path = 'https://image.tmdb.org/t/p/w500/';
+// 6ihyJWRLEngSnlW8HKeDOH3AfSQ.jpg
 
 class App extends React.Component {
   constructor(props){
@@ -36,14 +37,14 @@ class App extends React.Component {
 
   render () {
     return (
-      // <Container>
-      //   {
-      //     this.state.movies.map((movie, index) => (
-      //         <Header isEven={index % 2 === 0} key={movie.id}>{movie.title}</Header>
-      //       )
-      //     )
-      //   }
-      // </Container>
+      <PopularList>
+        {
+          this.state.movies.map((movie) => (
+              <Image key={movie.id} />
+            )
+          )
+        }
+      </PopularList>
     )
   }
 }
