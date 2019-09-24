ff152c6e57dad6c21b4b7a6750ce5a8b9634d6cf
unknown <hhe@mailfence.com>
2019-09-24 17:11:22 -0400

    got api response for popular tv_shows

--- src/App.js
+++ src/App.js
@@ -2,43 +2,48 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Container = styled.div
+`
   background: red;
   color: white;
 `;
 
-const Header = styled.h2`
+const Header = styled.h2
+`
   color: ${props => props.isEven ? 'green' : 'blue' };
 `;
 
-const api = 'https://api.themoviedb.org/3/discover/movie?api_key=d2788c89c4f55d19e63381c2d04593df&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+const api_key = 'd2788c89c4f55d19e63381c2d04593df';
+const api_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const api_tv = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US&page=1`;
 // https://image.tmdb.org/t/p/w500/6ihyJWRLEngSnlW8HKeDOH3AfSQ.jpg
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      movies: [] 
+      movies: [],
+      tvShow: []
     }
   }
 
   async componentDidMount(){
-
-    const response = await axios.get(api);
-    console.log(response);
+    const response = await axios.get(api_movies);
+    const response2 = await axios.get(api_tv);
     this.setState({ movies: response.data.results});
-    
+    this.setState({ tvShow: response2.data.results});
   }
 
   render () {
     return (
-      <Container>
-        {
-          this.state.movies.map((movie, index) => (
-            <Header isEven={index % 2 === 0} key={movie.id}>{movie.title}</Header>)
-          )
-        }
-      </Container>
+      // <Container>
+      //   {
+      //     this.state.movies.map((movie, index) => (
+      //         <Header isEven={index % 2 === 0} key={movie.id}>{movie.title}</Header>
+      //       )
+      //     )
+      //   }
+      // </Container>
     )
   }
 }
