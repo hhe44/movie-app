c8b74b219aa58ae527838538d626847a25452c47
unknown <hhe@mailfence.com>
2019-09-24 18:39:16 -0400

    rendered media-wrap as div capturing media image & name

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const PopularList = styled.div
-`
+const List = styled.div`
   background: #212025;
-  
+  display: flex;
+  flex-wrap: wrap;
 `;
 
-const Image = styled.img
+const MediaWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
-background-image: url("http://imageurlhere.com");
+
+const Image = styled.img`
+  width: 375px;
+  height: 211px;
+  background-image: url(${props => img_path + props.backdropPath});
+  background-size: 100%;
 `;
+
+const Caption = styled.h2 `
+  color: white;
+  display: none;
+`
 
 const api_key = 'd2788c89c4f55d19e63381c2d04593df';
 const api_movies = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
@@ -24,7 +38,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       movies: [],
-      tvShow: []
+      tvShows: []
     }
   }
 
@@ -32,20 +46,34 @@ class App extends React.Component {
     const response = await axios.get(api_movies);
     const response2 = await axios.get(api_tv);
     this.setState({ movies: response.data.results});
-    this.setState({ tvShow: response2.data.results});
+    this.setState({ tvShows: response2.data.results});
+    console.log(response2);
   }
 
   render () {
-    return (
-      <PopularList>
-        {
-          this.state.movies.map((movie) => (
-              <Image key={movie.id} />
-            )
-          )
-        }
-      </PopularList>
-    )
+    return ([
+
+      <List>
+      {
+        this.state.movies.map((movie) => [
+          <MediaWrap key={movie.id}>
+            <Image backdropPath={movie.backdrop_path}/>
+            <Caption>{movie.title}</Caption>
+          </MediaWrap>
+        ])
+      }
+      </List>,
+      <List>
+      {
+        this.state.tvShows.map((tvShow) => [
+          <MediaWrap key={tvShow.id}>
+            <Image backdropPath={tvShow.backdrop_path}/>
+            <Caption>{tvShow.name}</Caption>
+          </MediaWrap>
+        ])
+      }
+      </List>,
+    ])
   }
 }
 
