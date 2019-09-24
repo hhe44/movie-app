e0126b7b019f2efba871e259726a59d14f737144
unknown <hhe@mailfence.com>
2019-09-24 12:14:20 -0400

    setup work with Cristian

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  background: red;
+  color: white;
+`;
+
+const Header = styled.h2`
+  color: ${props => props.isEven ? 'green' : 'blue' };
+`;
+
+
+
+
+const api = 'https://api.themoviedb.org/3/discover/movie?api_key=d2788c89c4f55d19e63381c2d04593df&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: [] 
+    }
+  }
+
+  async componentDidMount(){
+    const response = await axios.get(api)
+
+    this.setState({ movies: response.data.results})
+  }
+
+  render () {
+    return (
+      <Container  >
+        {
+          this.state.movies.map((movie, index) => (
+          <Header
+            isEven={index % 2 === 0}
+            key={movie.id} 
+            >{movie.title}</Header>))
+        }
+        
+      </Container>
+
+    )
+  }
 }
 
 export default App;
