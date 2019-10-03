f26333350610570710a318e6e2602579df7a1aca
unknown <hhe@mailfence.com>
2019-10-03 16:50:02 -0400

    setting up api call to media by id

--- src/components/mediaPage.jsx
+++ src/components/mediaPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 const movieDetail = "https://api.themoviedb.org/3/movie/343611?api_key={api_key}";
 
 export default class MediaPage extends React.PureComponent {
+  state = {
+    media: []
+  };
 
-  // print = ({match}) => {
-  //   console.log(match);
-  // }
+  fetchMedia = async () => {
+    const getMediaDetail = `https://api.themoviedb.org/3/movie/343611?api_key=${process.env.REACT_APP_API_KEY}`;
+    const response = await axios.get(getMediaDetail);
+    this.setState({ media: response.data.results });
+    console.log(this.state.media)
+  };
 
-  print = () => { console.log(this.props) }
+  // Little debugger function here for help...!
+  print = () => { console.log(this.state); console.log(this.props.match.url); }
 
   render() {
     return (
