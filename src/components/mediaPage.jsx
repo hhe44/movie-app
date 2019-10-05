9dcaa40b1c1d15d3e1fca9bdcc6f3cc7908acb9a
unknown <hhe@mailfence.com>
2019-10-04 21:00:18 -0400

    let's try this for now and revert back if my idea doesn't work out

--- src/components/mediaPage.jsx
+++ src/components/mediaPage.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import axios from "axios";
 
-const movieDetail = "https://api.themoviedb.org/3/movie/343611?api_key={api_key}";
+const Container = styled.div`
+  box-sizing: border-box;
+  width: 100vw;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+`;
+
+const Poster = styled.img`
+  width: 94vw;
+  height: auto;
+`;
 
 export default class MediaPage extends React.PureComponent {
   state = {
     media: []
   };
 
-  
   async componentDidMount(){
     const param = this.props.match.url.split('/');
     const getMediaDetail = `https://api.themoviedb.org/3/${param[1]}/${param[2]}?api_key=${process.env.REACT_APP_API_KEY}`;
     const response = await axios.get(getMediaDetail);
     this.setState({media: response.data});
-    console.log(response);
+    console.log(response.data);
   }
   
   // fetchMedia = async () => {
@@ -29,10 +40,12 @@ export default class MediaPage extends React.PureComponent {
   print = () => { console.log(this.state); console.log(this.props.match.url); }
 
   render() {
+    const { media } = this.state;
+    const imagePath = "https://image.tmdb.org/t/p/w500";
     return (
-      <div> 
-        <h1>I AM RENDEEEERED</h1>
-      </div>
+      <Container>
+        <Poster src={imagePath+media.backdrop_path}></Poster>
+      </Container>
     );
   }
 }
